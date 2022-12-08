b9284fc3a605242a55df9abe8b2913cb45ef807d
Kai-Zhan Lee <kl2792@columbia.edu>
2022-12-08 16:26:43 -0500

    remove extraneous files

--- src/sh/submit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Generate experiment parameterizations and submit corresponding jobs.
-#
-
-src/sh/generate_experiments.sh > out/experiments.txt
-while read line; do sbatch src/sh/job.sh $line; done < out/experiments.txt
